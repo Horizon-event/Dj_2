ae65062e9854dd23d98a6575c88da6a0dca96c9a
Veter <Horizon-event@yandex.ru>
2021-12-13 11:12:16 +0300

    Плановое обновление в связи с обучением.

--- Base/views.py
+++ Base/views.py
@@ -13,7 +13,10 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        topic__name__icontains=q)  # фильтруются сообщения по темам, если нет конкретной темы, выводится все
     topics = Topic.objects.all()
 
     context = {'rooms': rooms, 'topics': topics}
